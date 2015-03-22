4b3c4b289574be9cdd9a4194190455a3ce57e47f
ishotjr <github@ishotjr.com>
2015-03-22 18:57:48 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# PW-DOS Command Line Watchface
+
+A simple spin on [Pebble's Watchface Tutorial](http://developer.getpebble.com/getting-started/watchface-tutorial/part2/); day and month are reflected in major and minor version numbers, and the files' timestamp shows the current time.
---
+++ src/PebbleTutorial1.c
@@ -0,0 +1,106 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+
+static GFont s_time_font;
+
+static BitmapLayer *s_starman_layer;
+static GBitmap *s_starman_bitmap;
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[150];// = "00:00";
+
+  // Write the current hours and minutes into the buffer
+
+  // TODO:
+  //if(clock_is_24h_style() == true) {
+
+  // TODO: this is horrendous/just very rough a prototype - refactor ASAP!
+  if(tick_time->tm_sec % 2) {
+    strftime(buffer, 150, "PW-DOS %d.%m\nCopyright (c) %Y\n\nAUTOEXEC BAT %H:%M\nCOMMAND  COM %H:%M\nCONFIG   SYS %H:%M\n 3 files %j bytes\n %U bytes free\n\nC:\\> ", tick_time);    
+  } else {
+    strftime(buffer, 150, "PW-DOS %d.%m\nCopyright (c) %Y\n\nAUTOEXEC BAT %H:%M\nCOMMAND  COM %H:%M\nCONFIG   SYS %H:%M\n 3 files %j bytes\n %U bytes free\n\nC:\\>_", tick_time);    
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorClear);
+  text_layer_set_overflow_mode(s_time_layer, GTextOverflowModeTrailingEllipsis);
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_14));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+  // Create GBitmap, then set to created BitmapLayer
+  s_starman_bitmap = gbitmap_create_with_resource(RESOURCE_ID_STARMAN);
+  s_starman_layer = bitmap_layer_create(GRect(104, 128, 40, 40));
+  bitmap_layer_set_bitmap(s_starman_layer, s_starman_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_starman_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+
+  // Destroy GBitmap
+  gbitmap_destroy(s_starman_bitmap);
+
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_starman_layer);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+  s_main_window = window_create();
+
+  // Set handlers to manage the elements inside the Window
+  window_set_window_handlers(s_main_window, (WindowHandlers) {
+    .load = main_window_load,
+    .unload = main_window_unload
+  });
+
+  // Show the Window on the watch, with animated=true
+  window_stack_push(s_main_window, true);
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
+  //tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+}
+
+static void deinit() {
+    // Destroy Window
+    window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
